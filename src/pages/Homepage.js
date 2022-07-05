76bbc2fc8d11c7821bea2d633bc0a3718341f696
duvalbarrett <duvalbarrett@gmail.com>
2022-07-05 10:39:10 -0400

    homepage changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,29 +7,21 @@ function Home() {
     <div className="home-container">
       <div className="topbanner"></div>
       <div className="ads-row-1">
+        <Link to="/phones">
         <img
           className={styles.ads1}
-          src="https://c2.staticflickr.com/4/3665/9499415587_3da844cd18_b.jpg"
+          src="https://i.pinimg.com/originals/31/93/90/319390e08180abe1a8bf4a63a92524fc.jpg"
           alt=""
         />
+        </Link>
       </div>
       <div className="ads-2-container">
         <div className="ad-text-3">
-          <Link to="/phones">
+          <Link to="/games">
             <img
               className={styles.ads2}
               img
-              src="https://i.pinimg.com/originals/31/93/90/319390e08180abe1a8bf4a63a92524fc.jpg"
-              alt=""
-            />
-          </Link>
-        </div>
-
-        <div className="ads-row-3">
-          <Link to="/games">
-            <img
-              className={styles.ads3}
-              src={require("../images/console.jpg")}
+              src={require('../images/console.jpg')}
               alt=""
             />
           </Link>
